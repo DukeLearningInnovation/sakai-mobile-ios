d9a5326d089fd6895377b38c761c5b19def6e0d7
niral28 <niralshah928@gmail.com>
2017-11-09 15:34:07 -0500

    Added key features closes #3

---
+++ DukeSakai/SiteIdDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SiteIdDelegate.swift
+//  DukeSakai
+//
+//  Created by Niral Shah on 11/6/17.
+//  Copyright © 2017 Zhe Mao. All rights reserved.
+//
+
+import Foundation
+protocol SiteIdDelegate:class {
+    func setSiteId(_siteIDstr:String)
+}
