6b088a3d13fd2cd133f2d907986628c6aa1fe78b
ash79 <ash79@duke.edu>
2019-03-11 07:51:36 -0400

    New Branch to Fix Issue

---
+++ DukeSakai/EmptyScreenVC.swift
@@ -0,0 +1,9 @@
+//
+//  EmptyScreenVC.swift
+//  DukeSakai
+//
+//  Created by Andres S. Hernandez G. on 2/22/19.
+//  Copyright © 2019 Zhe Mao. All rights reserved.
+//
+
+import Foundation
---
+++ DukeSakai/FeedbackVC.swift
@@ -0,0 +1,9 @@
+//
+//  FeedbackVC.swift
+//  DukeSakai
+//
+//  Created by Andres S. Hernandez G. on 3/1/19.
+//  Copyright © 2019 Zhe Mao. All rights reserved.
+//
+
+import Foundation
