ca624abae1182a78424df3172c81af40cb9d8f17
ash79 <ash79@duke.edu>
2019-04-29 16:51:52 -0400

    HTML parser function finalized

--- DukeSakai/EmptyScreenVC.swift
+++ DukeSakai/EmptyScreenVC.swift
@@ -22,21 +22,18 @@ class EmptyScreenVC: UIViewController{
         print( " _ts value is \(dateString)")
     }
 
+}
 
-
-    extension String {
-        var htmlToAttributedString: NSAttributedString? {
-            guard let data = data(using: .utf8) else { return NSAttributedString() }
-            do {
-                return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
-                } 
-            catch {
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
             return NSAttributedString()
-            }
-        }
-        var htmlToString: String {
-        return htmlToAttributedString?.string ?? ""
         }
     }
-
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
 }
