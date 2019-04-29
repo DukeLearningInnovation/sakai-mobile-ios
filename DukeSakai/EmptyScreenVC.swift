323f0ae0a424c1819a968256367c52cc1cec79e1
ash79 <ash79@duke.edu>
2019-04-29 16:47:43 -0400

    Added function to parse HTML into Strings, tested.

--- DukeSakai/EmptyScreenVC.swift
+++ DukeSakai/EmptyScreenVC.swift
@@ -8,4 +8,22 @@ class EmptyScreenVC: UIViewController{
         
         self.view.backgroundColor = UIColor.blue
     }
+
+
+
+    extension String {
+        var htmlToAttributedString: NSAttributedString? {
+            guard let data = data(using: .utf8) else { return NSAttributedString() }
+            do {
+                return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+                } 
+            catch {
+            return NSAttributedString()
+            }
+        }
+        var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+        }
+    }
+
 }
